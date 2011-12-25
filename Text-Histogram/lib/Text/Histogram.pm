8f1603094694659e6b12e339721d64ede96d6241
theMage <themage-mgksrc@fwd.avidmind.net>
2011-12-25 18:29:30 +0100

    module-starter autogenerated files
    themage

---
+++ Text-Histogram/lib/Text/Histogram.pm
@@ -0,0 +1,111 @@
+package Text::Histogram;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Text::Histogram - The great new Text::Histogram!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Text::Histogram;
+
+    my $foo = Text::Histogram->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Marco Neves, C<< <neves at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-text-histogram at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Text-Histogram>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Text::Histogram
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Text-Histogram>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Text-Histogram>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Text-Histogram>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Text-Histogram/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Marco Neves.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Text::Histogram
